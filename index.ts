830c26d5b80c9e96fd8ae622bf300e6217fc7c33
tahirbaba <muhammadtahirhasni@gmail.com>
2024-03-21 23:02:37 +0500

    minary changes and text The End

--- index.ts
+++ index.ts
@@ -4,8 +4,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number",name: "firstNumber",},
-  { message: "Enter second number", type: "number", name: "secondNumber" },
+  { message: "Enter your first number", type: "number",name: "firstNumber",},
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
   { message: "Select one of the operators to perform operation",
     type: "list",
     name: "operator",
@@ -25,3 +25,5 @@ if (answer.operator === "Addition") {
 } else {
     console.log("Please select valid operator");
 }
+
+console.log("The End");
